93208d8558a5145e7cdc85efdf9a90d7276e465e
Marcus Sanatan <msanatan@gmail.com>
2024-02-24 18:33:13 -0400

    Add some configs to the ingredients so we can customise them

--- ingredients/base_ingredient/base_ingredient.gd
+++ ingredients/base_ingredient/base_ingredient.gd
@@ -1,10 +1,13 @@
 class_name BaseIngredient
 extends Area2D
 
+@export var texture: Texture2D
+@export var ingredient: String
+@export var is_playing: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Sprite2D.texture = texture
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +16,5 @@ func _process(delta):
 
 
 func _on_input_event(viewport, event, shape_idx):
-	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.pressed:
+	if is_playing and (event is InputEventScreenTouch or event is InputEventMouseButton) and event.pressed:
 		print("Sprite touched")
