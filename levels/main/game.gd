21ce3c7f8ce673a32dd45ecf2481640f8ab129cd
Marcus Sanatan <msanatan@gmail.com>
2024-02-25 17:43:27 -0400

    Select toppings to find and add it to the pizza
    The pizza finally shows ingredeitns, and we keep track of what ingredients are the right ones.
    We currently select 1 topping for testing.

--- levels/main/game.gd
+++ levels/main/game.gd
@@ -15,9 +15,12 @@ enum GameState {READY, PLAYING, ROUND_OVER}
 
 var current_state: GameState = GameState.READY
 var ingredient_data: Array[IngredientData] = []
-var selected_ingredients: Array[Ingredient] = []
+var available_ingredients: Array[Ingredient] = []
+var available_ingredient_data: Array[IngredientData] = []
+var current_ingredeints: Array[IngredientData] = []
 var pizzas: Array[Pizza]
 var current_pizza: Pizza
+
 
 func _ready():
 	randomize()
@@ -31,10 +34,17 @@ func _input(event):
 	if current_state == GameState.READY and Input.is_action_just_pressed("click"):
 		current_state = GameState.PLAYING
 		ready_label.hide()
-		current_pizza.enter_scene()
 		# Make every ingredient active so they can react to the user selection
-		for ingredient in selected_ingredients:
+		for ingredient in available_ingredients:
 			ingredient.set_is_playing(true)
+		begin_round()
+
+
+## 
+func begin_round() -> void:
+	current_ingredeints = get_random_ingredient_data(1)
+	current_pizza.add_toppings(current_ingredeints)
+	current_pizza.enter_scene()
 
 
 ## Instiante ingredient scenes and adds them to the game
@@ -53,14 +63,19 @@ func setup_ingredients():
 		ingredient.ingredient_name = ingredient_data[i].name
 		ingredient.position = start_position + Vector2(x_offset, y_offset)
 		ingredients_container.add_child(ingredient)
-		selected_ingredients.append(ingredient)
+		available_ingredients.append(ingredient)
+		available_ingredient_data.append(ingredient_data[i])
 
 
+## Load pizzas and make the first one the active one
+## We're managing 2 pizzas so as the user gets one correct/wrong,
+## When it's leaving the screen, the new one is coming into the screen
 func setup_pizzas():
 	var children = $PizzaPool.get_children()
 	for child in children:
 		pizzas.append(child as Pizza)
 	current_pizza = pizzas[0]
+
 
 ## Loads the ingredients from the file system
 func load_resources_from_path() -> Array[IngredientData]:
@@ -80,10 +95,10 @@ func load_resources_from_path() -> Array[IngredientData]:
 
 ## This function returns a random selection of pizza ingredients.
 ## The pizza should be able to use ingredients that are available to the player.
-func get_random_ingredient_data(num_ingredients: int) -> Array[Ingredient]:
-	var chosen_ingredients = []
+func get_random_ingredient_data(num_ingredients: int) -> Array[IngredientData]:
+	var chosen_ingredients: Array[IngredientData] = []
 	while chosen_ingredients.size() < num_ingredients:
-		var sid = selected_ingredients.pick_random()
+		var sid = available_ingredient_data.pick_random()
 		var already_selected = false
 		for ci in chosen_ingredients:
 			if ci.name == sid.name:
--- pizza/pizza.gd
+++ pizza/pizza.gd
@@ -7,6 +7,14 @@ var original_position: Vector2
 
 func _ready():
 	original_position = position
+	
+
+## Show toppings on pizza
+func add_toppings(ingredients: Array[IngredientData]) -> void:
+	for ingredient in ingredients:
+		var ingredient_sprite = Sprite2D.new()
+		ingredient_sprite.texture = ingredient.pizza_texture
+		add_child(ingredient_sprite)
 
 
 ## Return the pizza to it's original state after it's used
