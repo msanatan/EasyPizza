a4e0558f34b96881b4165ec68d3422e97b7cbc37
Marcus Sanatan <msanatan@gmail.com>
2024-02-25 23:42:55 -0400

    Temporarily show the ingredient after clicking
    This should make it slightly easier on the player

--- ingredients/ingredient/ingredient.gd
+++ ingredients/ingredient/ingredient.gd
@@ -8,6 +8,7 @@ signal ingredient_selected
 @export var is_playing: bool = false
 
 @onready var cover_sprite: Sprite2D = $CoverSprite
+@onready var after_click_timer: Timer = $AfterClickTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +20,8 @@ func _on_input_event(viewport, event, shape_idx):
 	if is_playing and Input.is_action_just_pressed("click"):
 		print("%s was touched" % ingredient_name)
 		emit_signal("ingredient_selected", ingredient_name)
+		after_click_timer.start()
+		cover_sprite.hide()
 
 
 ## Toggle whether the ingredients are visible or covered
@@ -29,3 +32,8 @@ func set_is_playing(value: bool) -> void:
 	else:
 		cover_sprite.hide()
 		is_playing = false
+
+
+func _on_after_click_timer_timeout():
+	cover_sprite.show()
+	after_click_timer.stop()
